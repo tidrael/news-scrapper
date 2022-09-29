58ab4172cc45ba0c991e2b61f6d7944187e56a6c
tidrael <trieu@concurrently.dev>
2022-09-29 10:20:48 +0700

    update

--- script-start.sh
+++ script-start.sh
@@ -1,9 +1,12 @@
 sudo apt-get update -y
+sudo apt-get install -y wget
+wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
 sudo apt install python3-pip -y
-sudo pip3 install flask -y
+sudo pip3 install -r requirements.txt
 sudo apt-get install nginx -y
 sudo apt-get install gunicorn -y
-cp flaskapp.conf /etc/nginx/sites-enabled
+sudo cp flaskapp.conf /etc/nginx/sites-enabled
 sudo service nginx restart
 gunicorn app:app
 
@@ -12,7 +15,7 @@ gunicorn app:app
 # sudo python3 get-pip.py
 # sudo apt install -y gunicorn
 # sudo pip install -r requirements.txt
-# apt-get install -y wget
+# sudo apt-get install -y wget
 # wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 # sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
 # gunicorn --bind :8000 --workers 1 --threads 8 --timeout 0 app:app
